e6721750c352f88e94a044c6036f9fa42901559b
Bhosale Raje <111070382+rushikeshbhosale185@users.noreply.github.com>
2023-06-06 16:17:38 +0530

    Update init.js

--- js/init.js
+++ js/init.js
@@ -518,7 +518,7 @@ function arlo_tm_animate_text() {
   var animateSpan = jQuery(".arlo_tm_animation_text_word");
 
   animateSpan.typed({
-    strings: ["Flutter Developer", "Dart Developer", "Coder"],
+    strings: ["WordPress Developer", "Computer Hardware and Networking Engineer", "Coder" , "MERN Full Stack Web Developer"],
     loop: true,
     startDelay: 1e3,
     backDelay: 2e3,
